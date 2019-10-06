0128c61ae7f86ad0a636c5296364caa46c79a3dc
flynn <www439198341@gmail.com>
2019-10-06 14:58:13 +0800

    课程列表页完成

--- courses/urls.py
+++ courses/urls.py
@@ -1,3 +1,11 @@
+from courses.views import CourseListView
+
 __author__ = 'Flynn'
 __date__ = '2019/9/27 21:01'
 
+app_name = 'courses'
+from django.conf.urls import url
+
+urlpatterns = [
+    url('list/', CourseListView.as_view(), name='course_list'),
+]
--- courses/views.py
+++ courses/views.py
@@ -1,9 +1,34 @@
+from django.core.paginator import PageNotAnInteger
 from django.shortcuts import render
 
-# Create your views here.
 from django.views.generic.base import View
+from pure_pagination import Paginator
+
+from courses.models import Course
 
 
-class CourseView(View):
+class CourseListView(View):
     def get(self, request):
-        return render(request, 'org-list.html', {})
+        all_courses = Course.objects.all().order_by('-add_time')
+        hot_courses = all_courses.order_by('-click_nums')[:3]
+        sort = request.GET.get('sort', '')
+        if sort:
+            if sort == 'students':
+                all_courses = all_courses.order_by('-students')
+            elif sort == 'hot':
+                all_courses = all_courses.order_by('-click_nums')
+
+        # 分页处理
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+
+        p = Paginator(all_courses, 1, request=request)
+        courses = p.page(page)
+
+        return render(request, 'course-list.html', {
+            'all_courses': courses,
+            'hot_courses': hot_courses,
+            'sort': sort,
+        })
